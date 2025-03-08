f78380178c9bb72984e5a0bc374f6583ab91fda0
UrsusMortiferum <101043729+UrsusMortiferum@users.noreply.github.com>
2025-03-08 09:19:20 +0100

    added "count of words"

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 def get_boot_text(file_path):
     with open(file_path) as file:
-        contents = file.read()
-        return contents
+        content = file.read()
+        return content
+
+def get_num_words(content):
+    words = content.split()
+    return len(words)
 
 def main():
     file_path = "./books/frankenstein.txt"
-    print(get_boot_text(file_path))
+    # print(get_boot_text(file_path))
+    content = get_boot_text(file_path)
+    num_words = get_num_words(content)
+    print(f"{num_words} words found in the document")
 
 main()
